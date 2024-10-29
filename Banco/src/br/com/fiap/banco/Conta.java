14e7a9f0eb35047ee2be07b3d5757eade21e4cd6
ErosMachado <erosrodriguesmachado@gmail.com>
2024-10-29 17:33:32 -0300

    getters and setters

--- Banco/src/br/com/fiap/banco/Conta.java
+++ Banco/src/br/com/fiap/banco/Conta.java
@@ -1,10 +1,10 @@
 package br.com.fiap.banco;
 
 public class Conta {
-	String nomeCliente;
-	String cpfCliente;
-	int numeroConta;
-	double saldo;
+	private String nomeCliente;
+	private String cpfCliente;
+	private int numeroConta;
+	private double saldo;
 	
 	
 	//constructor ctrl + 3 (generate constructor)
@@ -15,12 +15,45 @@ public class Conta {
 		this.numeroConta = numeroConta;
 		this.saldo = saldo;
 	}
-
-
+	
+	//metodos ctrl + 3 (getters and setters)
 	
 	
-	
 
+
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getCpfCliente() {
+		return cpfCliente;
+	}
+
+	public void setCpfCliente(String cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
 	public boolean sacar(double valor) {
 		if (saldo >= valor) {
 			saldo -= valor;
@@ -28,6 +61,7 @@ public class Conta {
 		}
 		return false;
 	}
+
 
 	public void depositar(double valor) {
 		saldo += valor;
--- Banco/src/br/com/fiap/banco/Executavel.java
+++ Banco/src/br/com/fiap/banco/Executavel.java
@@ -1,21 +1,24 @@
 package br.com.fiap.banco;
 
 public class Executavel {
-    public static void main(String[] args) {
-    	
-        Conta conta1 = new Conta("Eros Machado", "123.456.789-00", 1001, 1000.00);
-        Conta conta2 = new Conta("João Silva", "987.654.321-00", 1002, 2000.00);
-        Conta conta3 = new Conta("Maria Oliveira", "111.222.333-44", 1003, 3000.00);
-        
-        conta1.consultarSaldo();
-        conta1.sacar(300.00);
-        conta1.consultarSaldo();
-        
-        conta2.depositar(500.00);
-        conta2.consultarSaldo();
-        
-        conta3.transferir(conta1, 1000.00);
-        conta1.consultarSaldo();
-        conta3.consultarSaldo();
-    }
+	public static void main(String[] args) {
+
+		Conta conta1 = new Conta("Eros Machado", "123.456.789-00", 1001, 1000.00);
+		Conta conta2 = new Conta("João Silva", "987.654.321-00", 1002, 2000.00);
+		Conta conta3 = new Conta("Maria Oliveira", "111.222.333-44", 1003, 3000.00);
+
+		conta1.consultarSaldo();
+		conta1.sacar(300.00);
+		conta1.consultarSaldo();
+
+		conta2.depositar(500.00);
+		conta2.consultarSaldo();
+
+		conta3.transferir(conta1, 1000.00);
+		conta1.consultarSaldo();
+		conta3.consultarSaldo();
+
+		conta1.saldo = 100000000;
+
+	}
 }
